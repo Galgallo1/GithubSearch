a47604bcb6fdae92c5b5db88ddd3349a47f3ba53
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-15 08:44:09 +0300

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UsersComponent } from './users/users.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    UsersComponent,
+    NotFoundComponent,
+    NavBarComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
